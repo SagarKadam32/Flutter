78c3621b278ad30701f98f6152f03bc5a016d780
Sagar Kadam <misc.sagarkadam@gmail.com>
2023-02-12 17:52:41 +0530

    Update main.dart
    Center Image and created a Scaffold with proerties background color and app bar and image title with image fetched from url

--- i_am_rich/lib/main.dart
+++ i_am_rich/lib/main.dart
@@ -2,9 +2,19 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    const MaterialApp(
-      home: Center(
-        child: Text('Hello World'),
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.blueGrey,
+        appBar: AppBar(
+          title: Text('I Am Rich'),
+          backgroundColor: Colors.blueGrey[900],
+        ),
+        body: Center(
+          child: Image(
+            image: NetworkImage(
+                'http://www.architecture-student.com/wp-content/uploads/2011/08/final-site.jpg'),
+          ),
+        ),
       ),
     ),
   );
