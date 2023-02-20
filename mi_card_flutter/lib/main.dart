deb872cd216fd4e4d99d012a0db1014f2e7ec3a4
Sagar Kadam <misc.sagarkadam@gmail.com>
2023-02-20 18:47:25 +0530

    Code Update - Font updation & New Images

--- mi_card_flutter/lib/main.dart
+++ mi_card_flutter/lib/main.dart
@@ -13,18 +13,71 @@ class MyApp extends StatelessWidget {
         body: SafeArea(
           child: Column(
             children: [
-              CircleAvatar(
-                radius: 50,
-                backgroundImage: AssetImage('images/Tom.jpeg'),
-              ),
-              Text(
+              const CircleAvatar(
+                  radius: 50, backgroundImage: AssetImage('images/Tom.jpeg')),
+              const Text(
                 'Tom Cruise',
                 style: TextStyle(
                     fontFamily: 'Pacifico',
                     fontSize: 40,
                     color: Colors.white,
                     fontWeight: FontWeight.bold),
-              )
+              ),
+              const Text(
+                'Chief Mission Architect',
+                style: TextStyle(
+                    fontFamily: 'SourceSansPro',
+                    fontSize: 20,
+                    color: Colors.white,
+                    fontWeight: FontWeight.normal,
+                    letterSpacing: 2.5),
+              ),
+              Container(
+                color: Colors.white,
+                padding: EdgeInsets.all(10.0),
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                child: Row(
+                  children: [
+                    Icon(
+                      Icons.phone,
+                      color: Colors.teal,
+                    ),
+                    SizedBox(
+                      width: 10.0,
+                    ),
+                    Text(
+                      '+91 9876123423',
+                      style: TextStyle(
+                          fontFamily: 'Source Sans Pro',
+                          color: Colors.teal.shade900,
+                          fontSize: 20.0),
+                    ),
+                  ],
+                ),
+              ),
+              Container(
+                color: Colors.white,
+                padding: EdgeInsets.all(10.0),
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                child: Row(
+                  children: [
+                    Icon(
+                      Icons.email,
+                      color: Colors.teal,
+                    ),
+                    SizedBox(
+                      width: 10.0,
+                    ),
+                    Text(
+                      'tom.cruise@maveric.com',
+                      style: TextStyle(
+                          fontFamily: 'Source Sans Pro',
+                          color: Colors.teal.shade900,
+                          fontSize: 20.0),
+                    )
+                  ],
+                ),
+              ),
             ],
           ),
         ),
