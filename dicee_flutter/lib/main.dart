45e9608bf6985da343afb5a0a67ff6cb91409e65
Sagar Kadam <misc.sagarkadam@gmail.com>
2023-02-27 20:17:00 +0530

    Generation of the random no on click of any dice.

--- dicee_flutter/lib/main.dart
+++ dicee_flutter/lib/main.dart
@@ -38,6 +38,7 @@ class _DicePageState extends State<DicePage> {
             onPressed: () {
               setState(() {
                 leftDiceNumber = Random().nextInt(6) + 1;
+                rightDiceNumber = Random().nextInt(6) + 1;
                 print('Left Dice Number $leftDiceNumber');
               });
             },
@@ -48,6 +49,7 @@ class _DicePageState extends State<DicePage> {
               onPressed: () {
                 setState(() {
                   rightDiceNumber = Random().nextInt(6) + 1;
+                  leftDiceNumber = Random().nextInt(6) + 1;
                   print('Right Dice Number $rightDiceNumber');
                 });
               },
