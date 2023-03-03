54f1a4e1128e1d9119d51367ae72628eca0e100b
Sagar Kadam <misc.sagarkadam@gmail.com>
2023-03-03 18:38:54 +0530

    Initial Code - Xylophone Flutter Project
    Added the English word package
    Added the Assets folder and wave files.

---
+++ xylophone_flutter/lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
+
+void main() {
+  runApp(XylophoneApp());
+}
+
+class XylophoneApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: SafeArea(
+          child: Container(),
+        ),
+      ),
+    );
+  }
+}
