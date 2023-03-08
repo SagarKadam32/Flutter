7bac35d55d7c5ff489a527eeebfba6966f61eb76
Sagar Kadam <misc.sagarkadam@gmail.com>
2023-03-08 13:39:35 +0530

    Aligned the buttons to default left.

--- xylophone_flutter/lib/main.dart
+++ xylophone_flutter/lib/main.dart
@@ -12,110 +12,107 @@ class XylophoneApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         body: SafeArea(
-          child: Center(
-            child: Container(
-              child: Column(
-                children: [
-                  TextButton(
-                    onPressed: () {
-                      final player = AudioPlayer();
-                      player.setSource(AssetSource('note1.wav'));
-                      player.setVolume(8.0);
-                      player.resume();
-                      player.play(AssetSource('note1.wav'));
-                    },
-                    style: TextButton.styleFrom(
-                      backgroundColor: Colors.red, // Background Color
-                    ),
-                    child: Text(''),
+          child: Container(
+            child: Column(
+              children: [
+                TextButton(
+                  onPressed: () {
+                    final player = AudioPlayer();
+                    player.setSource(AssetSource('note1.wav'));
+                    player.setVolume(8.0);
+                    player.resume();
+                    player.play(AssetSource('note1.wav'));
+                  },
+                  style: TextButton.styleFrom(
+                    backgroundColor: Colors.red, // Background Color
                   ),
-                  const SizedBox(height: 5),
-                  TextButton(
-                    onPressed: () {
-                      final player = AudioPlayer();
-                      player.setSource(AssetSource('note2.wav'));
-                      player.setVolume(8.0);
-                      player.resume();
-                      player.play(AssetSource('note2.wav'));
-                    },
-                    style: TextButton.styleFrom(
-                      backgroundColor: Colors.orange, // Background Color
-                    ),
-                    child: Text(''),
+                  child: Text(''),
+                ),
+                const SizedBox(height: 5),
+                TextButton(
+                  onPressed: () {
+                    final player = AudioPlayer();
+                    player.setSource(AssetSource('note2.wav'));
+                    player.setVolume(8.0);
+                    player.resume();
+                    player.play(AssetSource('note2.wav'));
+                  },
+                  style: TextButton.styleFrom(
+                    backgroundColor: Colors.orange, // Background Color
                   ),
-                  const SizedBox(height: 5),
-                  TextButton(
-                    onPressed: () {
-                      final player = AudioPlayer();
-                      player.setSource(AssetSource('note3.wav'));
-                      player.setVolume(8.0);
-                      player.resume();
-                      player.play(AssetSource('note3.wav'));
-                    },
-                    style: TextButton.styleFrom(
-                      backgroundColor: Colors.yellow, // Background Color
-                    ),
-                    child: Text(''),
+                  child: Text(''),
+                ),
+                const SizedBox(height: 5),
+                TextButton(
+                  onPressed: () {
+                    final player = AudioPlayer();
+                    player.setSource(AssetSource('note3.wav'));
+                    player.setVolume(8.0);
+                    player.resume();
+                    player.play(AssetSource('note3.wav'));
+                  },
+                  style: TextButton.styleFrom(
+                    backgroundColor: Colors.yellow, // Background Color
                   ),
-                  const SizedBox(height: 5),
-                  TextButton(
-                    onPressed: () {
-                      final player = AudioPlayer();
-                      player.setSource(AssetSource('note4.wav'));
-                      player.setVolume(8.0);
-                      player.resume();
-                      player.play(AssetSource('note4.wav'));
-                    },
-                    style: TextButton.styleFrom(
-                      backgroundColor: Colors.green, // Background Color
-                    ),
-                    child: Text(''),
+                  child: Text(''),
+                ),
+                const SizedBox(height: 5),
+                TextButton(
+                  onPressed: () {
+                    final player = AudioPlayer();
+                    player.setSource(AssetSource('note4.wav'));
+                    player.setVolume(8.0);
+                    player.resume();
+                    player.play(AssetSource('note4.wav'));
+                  },
+                  style: TextButton.styleFrom(
+                    backgroundColor: Colors.green, // Background Color
                   ),
-                  const SizedBox(height: 5),
-                  TextButton(
-                    onPressed: () {
-                      final player = AudioPlayer();
-                      player.setSource(AssetSource('note5.wav'));
-                      player.setVolume(8.0);
-                      player.resume();
-                      player.play(AssetSource('note5.wav'));
-                    },
-                    style: TextButton.styleFrom(
-                      backgroundColor:
-                          Colors.green.shade800, // Background Color
-                    ),
-                    child: Text(''),
+                  child: Text(''),
+                ),
+                const SizedBox(height: 5),
+                TextButton(
+                  onPressed: () {
+                    final player = AudioPlayer();
+                    player.setSource(AssetSource('note5.wav'));
+                    player.setVolume(8.0);
+                    player.resume();
+                    player.play(AssetSource('note5.wav'));
+                  },
+                  style: TextButton.styleFrom(
+                    backgroundColor: Colors.green.shade800, // Background Color
                   ),
-                  const SizedBox(height: 5),
-                  TextButton(
-                    onPressed: () {
-                      final player = AudioPlayer();
-                      player.setSource(AssetSource('note6.wav'));
-                      player.setVolume(8.0);
-                      player.resume();
-                      player.play(AssetSource('note6.wav'));
-                    },
-                    style: TextButton.styleFrom(
-                      backgroundColor: Colors.blue, // Background Color
-                    ),
-                    child: Text(''),
+                  child: Text(''),
+                ),
+                const SizedBox(height: 5),
+                TextButton(
+                  onPressed: () {
+                    final player = AudioPlayer();
+                    player.setSource(AssetSource('note6.wav'));
+                    player.setVolume(8.0);
+                    player.resume();
+                    player.play(AssetSource('note6.wav'));
+                  },
+                  style: TextButton.styleFrom(
+                    backgroundColor: Colors.blue, // Background Color
                   ),
-                  const SizedBox(height: 5),
-                  TextButton(
-                    onPressed: () {
-                      final player = AudioPlayer();
-                      player.setSource(AssetSource('note7.wav'));
-                      player.setVolume(8.0);
-                      player.resume();
-                      player.play(AssetSource('note7.wav'));
-                    },
-                    style: TextButton.styleFrom(
-                      backgroundColor: Colors.purple, // Background Color
-                    ),
-                    child: Text(''),
+                  child: Text(''),
+                ),
+                const SizedBox(height: 5),
+                TextButton(
+                  onPressed: () {
+                    final player = AudioPlayer();
+                    player.setSource(AssetSource('note7.wav'));
+                    player.setVolume(8.0);
+                    player.resume();
+                    player.play(AssetSource('note7.wav'));
+                  },
+                  style: TextButton.styleFrom(
+                    backgroundColor: Colors.purple, // Background Color
                   ),
-                ],
-              ),
+                  child: Text(''),
+                ),
+              ],
             ),
           ),
         ),
