f3db544dedf2476bb17cf34d1481ef91dd1ea9f6
Sagar Kadam <misc.sagarkadam@gmail.com>
2023-03-12 20:37:30 +0530

    Completed the next TODO items.
    //TODO: Step 6 - import the story.dart file into this file.
    
    //TODO: Step 5 - Create a new class called StoryBrain.
    //TODO: Step 7 - Uncomment the lines below to include storyData as a private property in StoryBrain. Hint: You might need to change something in story.dart to make this work.

--- destini_challenge/lib/story.dart
+++ destini_challenge/lib/story.dart
@@ -1,11 +1,7 @@
 class Story {
   String storyTitle = '';
-  String choiceText1 = '';
-  String choiceText2 = '';
-
-  Story(this.storyTitle, this.choiceText1, this.choiceText2);
+  String choice1 = '';
+  String choice2 = '';
+  Story(
+      {required this.storyTitle, required this.choice1, required this.choice2});
 }
-
-//TODO: Step 2 - Create a new class called Story.
-//TODO: Step 3 - Create 3 properties for this class, A. storyTitle to store the story text. B. choice1 to store the text for choice 1, C. choice2 to store the text for choice 2.
-//TODO: Step 4 - Create a Constructor for this class to be able to initialise the properties created in step 3.
