ee7d7658505de9dca55f25f0f454bad9143280fc
Sagar Kadam <misc.sagarkadam@gmail.com>
2023-03-13 18:43:26 +0530

    Hide the Second button on the story

--- destini_challenge/lib/story_brain.dart
+++ destini_challenge/lib/story_brain.dart
@@ -74,6 +74,14 @@ class StoryBrain {
   void restart() {
     _storyNumber = 0;
   }
+
+  bool buttonShouldBeVisible() {
+    bool buttonVisibility = true;
+    if (_storyNumber == 4 || _storyNumber == 5) {
+      buttonVisibility = false;
+    }
+    return buttonVisibility;
+  }
 }
 
 //TODO: Step 23 - Use the storyNumber property inside getStory(), getChoice1() and getChoice2() so that it gets the updated story and choices rather than always just the first (0th) one.
