776d68575e4a27c9204a710127b86bed6006bd9a
Sagar Kadam <misc.sagarkadam@gmail.com>
2023-04-02 21:00:36 +0530

    Added the http network package

--- clima_flutter/lib/screens/loading_screen.dart
+++ clima_flutter/lib/screens/loading_screen.dart
@@ -1,5 +1,10 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
-import 'package:geolocator/geolocator.dart';
+import 'package:clima_flutter/services/location.dart';
+import 'package:http/http.dart' as http;
+
+const apiKey = 'ce1a9cc18151f66808efbf60cd527aaf';
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -8,9 +13,10 @@ class LoadingScreen extends StatefulWidget {
 
 class _LoadingScreenState extends State<LoadingScreen> {
   void getLocation() async {
-    Position position = await Geolocator.getCurrentPosition(
-        desiredAccuracy: LocationAccuracy.low);
-    print(position);
+    Location location = Location();
+    await location.getCurrentLocation();
+    print(location.longitude);
+    print(location.latitude);
   }
 
   @override
@@ -18,6 +24,34 @@ class _LoadingScreenState extends State<LoadingScreen> {
     // TODO: implement initState
     super.initState();
     getLocation();
+    getData();
+  }
+
+  void getData() async {
+    //  http.Response response = await http.get(
+    //  'https://api.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=$apiKey');
+
+    var url = Uri.https('api.openweathermap.org', '/data/2.5/weather',
+        {'lat': '35', 'lon': '139', 'appid': '$apiKey'});
+
+    // Await the http get response, then decode the json-formatted response.
+    var response = await http.get(url);
+
+    print('Response status: ${response.statusCode}');
+    print('Response body: ${response.body}');
+
+    if (response.statusCode == 200) {
+      String data = response.body;
+
+      var decodedData = jsonDecode(data);
+      double temperature = decodedData['main']['temp'];
+      int condition = decodedData['weather'][0]['id'];
+      String cityName = decodedData['name'];
+
+      print(temperature);
+      print(condition);
+      print(cityName);
+    }
   }
 
   @override
