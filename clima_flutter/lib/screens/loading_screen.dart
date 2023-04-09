dc602b9666e665b4859d24c87a8a7b80cf3eb7d5
Sagar Kadam <misc.sagarkadam@gmail.com>
2023-04-09 16:41:59 +0530

    Added the display of temperature and location city to UI

--- clima_flutter/lib/screens/loading_screen.dart
+++ clima_flutter/lib/screens/loading_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:clima_flutter/services/location.dart';
 import 'package:clima_flutter/services/networking.dart';
+import 'package:clima_flutter/screens/location_screen.dart';
 
 const apiKey = 'ce1a9cc18151f66808efbf60cd527aaf';
 
@@ -17,19 +18,28 @@ class _LoadingScreenState extends State<LoadingScreen> {
     await location.getCurrentLocation();
     latitude = location.latitude;
     longitude = location.longitude;
-    NetworkHelper networkHelper = NetworkHelper(
-        'api.openweathermap.org',
-        '/data/2.5/weather',
-        {'lat': '$latitude', 'lon': '$longitude', 'appid': '$apiKey'});
+    NetworkHelper networkHelper =
+        NetworkHelper('api.openweathermap.org', '/data/2.5/weather', {
+      'lat': '$latitude',
+      'lon': '$longitude',
+      'appid': '$apiKey',
+      'units': 'metric',
+    });
 
     var weatherData = await networkHelper.getData();
-    double temperature = weatherData['main']['temp'];
-    int condition = weatherData['weather'][0]['id'];
-    String cityName = weatherData['name'];
+    // double temperature = weatherData['main']['temp'];
+    // int condition = weatherData['weather'][0]['id'];
+    // String cityName = weatherData['name'];
 
-    print(temperature);
-    print(condition);
-    print(cityName);
+    // print(temperature);
+    // print(condition);
+    // print(cityName);
+
+    Navigator.push(context, MaterialPageRoute(builder: (context) {
+      return LocationScreen(
+        locationWeather: weatherData,
+      );
+    }));
   }
 
   @override
--- clima_flutter/lib/screens/location_screen.dart
+++ clima_flutter/lib/screens/location_screen.dart
@@ -2,11 +2,36 @@ import 'package:flutter/material.dart';
 import '../utilities/constants.dart';
 
 class LocationScreen extends StatefulWidget {
+  LocationScreen({this.locationWeather});
+  final locationWeather;
+
   @override
   _LocationScreenState createState() => _LocationScreenState();
 }
 
 class _LocationScreenState extends State<LocationScreen> {
+  late int temperature;
+  late int condition;
+  late String cityName;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    updateUI(widget.locationWeather);
+  }
+
+  void updateUI(dynamic weatherData) {
+    double temp = weatherData['main']['temp'];
+    temperature = temp.toInt();
+    condition = weatherData['weather'][0]['id'];
+    cityName = weatherData['name'];
+
+    print(temperature);
+    // print(condition);
+    // print(cityName);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -49,11 +74,11 @@ class _LocationScreenState extends State<LocationScreen> {
                 child: Row(
                   children: <Widget>[
                     Text(
-                      '32°',
+                      '$temperature°',
                       style: kTempTextStyle,
                     ),
                     Text(
-                      '☀️',
+                      temperature < 10 ? '☃️' : '☀️',
                       style: kConditionTextStyle,
                     ),
                   ],
@@ -62,7 +87,7 @@ class _LocationScreenState extends State<LocationScreen> {
               Padding(
                 padding: EdgeInsets.only(right: 15.0),
                 child: Text(
-                  "It's 🍦 time in San Francisco!",
+                  "It's 🍦 time in $cityName!",
                   textAlign: TextAlign.right,
                   style: kMessageTextStyle,
                 ),
