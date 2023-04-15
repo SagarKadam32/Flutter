a632723433c6b3276d9576097e022b6cb6e02586
Sagar Kadam <misc.sagarkadam@gmail.com>
2023-04-15 15:24:40 +0530

    Used the For Each Statement

--- bitcoin_ticker_flutter/lib/price_screen.dart
+++ bitcoin_ticker_flutter/lib/price_screen.dart
@@ -11,9 +11,7 @@ class _PriceScreenState extends State<PriceScreen> {
 
   List<DropdownMenuItem> getDropdownItems() {
     List<DropdownMenuItem<String>> dropDownItems = [];
-
-    for (int i = 0; i < currenciesList.length; i++) {
-      String currency = currenciesList[i];
+    for (String currency in currenciesList) {
       var newItem = DropdownMenuItem(
         value: currency,
         child: Text(currency),
